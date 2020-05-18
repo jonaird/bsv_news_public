2ac6eda7192db9feb2b986254b56b2225a0f65d2
Jonathan Aird <jonathan.aird@gmail.com>
2020-05-18 17:56:54 +0300

    ...

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "24b0ec6fe3c016984f1ad22b4e442a4f",
 "/": "24b0ec6fe3c016984f1ad22b4e442a4f",
-"main.dart.js": "0143cc50a31297705cd9df7ccd26ab45",
+"main.dart.js": "2b75215f74253c78adce6f67d4dcff74",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
